b3e5927a275f4c93c7c0dcdfc01da1d4743834f4
Gary Lee <jakarta99@gmail.com>
2022-03-10 13:30:55 +0800

    init project

---
+++ src/main/java/tw/com/fcb/lion/core/FcbCoreApplication.java
@@ -0,0 +1,13 @@
+package tw.com.fcb.lion.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcbCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcbCoreApplication.class, args);
+	}
+
+}
