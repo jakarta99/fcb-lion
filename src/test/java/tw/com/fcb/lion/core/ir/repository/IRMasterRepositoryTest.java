7d869bf7fa638729f017b2020193c90dc90d832a
iris <dragoniris7@gmail.com>
2022-03-16 15:22:13 +0800

    IRMasterRepositoryTest commit

---
+++ src/test/java/tw/com/fcb/lion/core/ir/repository/IRMasterRepositoryTest.java
@@ -0,0 +1,14 @@
+package tw.com.fcb.lion.core.ir.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class IRMasterRepositoryTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
