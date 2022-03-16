5d2c130c92aaf7ae5436086c0fa2c5f0a0a47e25
copykobe0103 <copykobe@gmail.com>
2022-03-16 16:12:03 +0800

    findByBeAdvisingBranchAndPrintAdvisingMK unit test

--- src/test/java/tw/com/fcb/lion/core/ir/repository/IRMasterRepositoryTest.java
+++ src/test/java/tw/com/fcb/lion/core/ir/repository/IRMasterRepositoryTest.java
@@ -3,12 +3,27 @@ package tw.com.fcb.lion.core.ir.repository;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tw.com.fcb.lion.core.ir.repository.entity.IRMaster;
+import tw.com.fcb.lion.core.ir.web.dto.IR;
 
+@SpringBootTest
 class IRMasterRepositoryTest {
+
+	@Autowired
+	IRMasterRepository irMasterRepository;
 
 	@Test
 	void test() {
 		fail("Not yet implemented");
 	}
 
+
+	@Test
+	void findByBeAdvisingBranchAndPrintAdvisingMk() {
+		Integer caseCunt = irMasterRepository.findByBeAdvisingBranchAndPrintAdvisingMk("094","N").size();
+		assertEquals(caseCunt,3);
+
+	}
 }
