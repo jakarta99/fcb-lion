70a720951b15e46b0cda4db3b8d587aaf089a038
LAPTOP-6AEJPV82\user <a24562327@gmail.com>
2022-03-16 10:52:39 +0800

    weifan

--- src/main/java/tw/com/fcb/lion/core/ir/repository/IRMasterRepository.java
+++ src/main/java/tw/com/fcb/lion/core/ir/repository/IRMasterRepository.java
@@ -2,6 +2,7 @@ package tw.com.fcb.lion.core.ir.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import tw.com.fcb.lion.core.ir.repository.entity.IRMaster;
 
@@ -13,6 +14,6 @@ public interface IRMasterRepository extends JpaRepository <IRMaster,Long> {
     List<IRMaster> findByBeAdvisingBranch(String beAdvisingBranch);
     
 	//以外匯編號查詢匯入主檔
-    @Query(name="findByirNo",nativeQuery = true,value = "select * from IR_MASTER where IR_NO = :irNo")
-	IRMaster findByirNo(String irNo);
+    //@Query(name="findByirNo",nativeQuery = true,value = "select * from IR_MASTER where IR_NO = :irNo")
+	IRMaster findByirNo(@Param("irNo") String irNo);
 }
