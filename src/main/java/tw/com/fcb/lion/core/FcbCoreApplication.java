4384836fd998ce1a9eb2fa6af1744687254677c1
copykobe0103 <copykobe@gmail.com>
2022-04-22 12:38:16 +0800

    暫解FP FeignClients問題，加上@ComponentScan

--- src/main/java/tw/com/fcb/lion/core/FcbCoreApplication.java
+++ src/main/java/tw/com/fcb/lion/core/FcbCoreApplication.java
@@ -3,9 +3,11 @@ package tw.com.fcb.lion.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableFeignClients
+@ComponentScan({"tw.com.fcb.fp.core.fp.web.FPClient"})
 public class FcbCoreApplication {
 
 	public static void main(String[] args) {
